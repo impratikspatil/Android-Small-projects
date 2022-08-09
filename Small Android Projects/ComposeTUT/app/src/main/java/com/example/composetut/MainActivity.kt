4564092f06ecdd35d362f62953cd1f7bfbcc167f
impratikspatil <impratikspatil@gmail.com>
2022-08-09 17:39:58 +0530

    commit

--- Small Android Projects/ComposeTUT/app/src/main/java/com/example/composetut/MainActivity.kt
+++ Small Android Projects/ComposeTUT/app/src/main/java/com/example/composetut/MainActivity.kt
@@ -10,17 +10,15 @@ import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,8 +33,39 @@ class MainActivity : ComponentActivity() {
             Column {
                 for(i in 1..6)
                 {
-                    Greeting(name = "prasad $i")
+                    Greeting(name = "pratik $i")
                 }
+                Button(
+                    onClick = { /* ... */ },
+
+                    contentPadding = PaddingValues(
+                        start = 20.dp,
+                        top = 12.dp,
+                        end = 20.dp,
+                        bottom = 12.dp
+                    )
+                ) {
+                    // Inner content including an icon and a text label
+                    Icon(
+                        Icons.Filled.Favorite,
+                        contentDescription = "Favorite",
+                        modifier = Modifier.size(ButtonDefaults.IconSize)
+                    )
+                    Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                    Text("Like")
+                }
+
+
+                ExtendedFloatingActionButton(
+                    onClick = { /* ... */ },
+                    icon = {
+                        Icon(
+                            Icons.Filled.Favorite,
+                            contentDescription = "Favorite"
+                        )
+                    },
+                    text = { Text("Like") }
+                )
 
             }
 
@@ -48,33 +77,31 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Row(modifier = Modifier
-        .padding(10.dp)
+        .padding(10.dp),
+        verticalAlignment =Alignment.CenterVertically
     ) {
         Image(painter = painterResource(id = R.drawable.crop), contentDescription ="pratik image",
             modifier = Modifier
-
                 .size(60.dp)
                 .clip(CircleShape)
                 .border(2.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
 
         Spacer(modifier = Modifier.width(10.dp))
-        if (LocalInspectionMode.current) {
-            // Show this text in a preview window:
-            Text("Hello preview user!")
-        } else {
-            // Show this text in the app:
-            Text("Hello $name!")
-        }
+        Text(
+            text=name,
+            style = MaterialTheme.typography.h6
+
+
+        )
 
     }
 }
 
-@Preview(showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeTUTTheme {
         Greeting("Android")
     }
 }
-
